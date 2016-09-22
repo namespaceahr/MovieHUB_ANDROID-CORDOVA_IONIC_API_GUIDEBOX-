6db9489d25358ff95aa89bc1402cb4a4e0e346a5
Rifat Ahmed Habibullah <ahrlab@live.com>
2016-09-22 19:39:18 +0600

    Network Issue Fixed
    Network Issue Fixed.Network Detection.

--- MovieHub/www/js/app.js
+++ MovieHub/www/js/app.js
@@ -7,7 +7,8 @@ angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+      
+         if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -16,9 +17,10 @@ angular.module('starter', ['ionic'])
       // from snapping when text inputs are focused. Ionic handles this internally for
       // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
+      }
+      if(window.StatusBar) {
       StatusBar.styleDefault();
     }
-  });
+});
 })
+
--- MovieHub/www/js/movieController.js
+++ MovieHub/www/js/movieController.js
@@ -1,9 +1,49 @@
-angular.module('myAPP', ['ionic'])
-.controller('MovieController', function($scope, $http,$ionicPopup, $timeout) {
-     $http.get("https://api-public.guidebox.com/v1.43/US/mBgbFjzVsdUCxXM8C0dpFs9ICQB0ZJ/movies/all/50/25/all/all")
+var app=angular.module('myAPP', ['ionic']);
+app.controller('MovieController', function($ionicPlatform,$scope, $http,$ionicPopup,$interval) {
+	var isConnectionLost=false;
+	var isConnectError=false;
+	
+     if(navigator.onLine)
+	 {
+	$http.get("https://api-public.guidebox.com/v1.43/US/mBgbFjzVsdUCxXM8C0dpFs9ICQB0ZJ/movies/all/50/25/all/all")
     .then(function(response) {
-		
         $scope.searchResults=response.data.results;
     });
-
+	 }
+	 else{
+		  OffLine();
+		  isConnectError=true;
+	 }
+	 
+	 $interval(function () {
+        if(navigator.onLine)
+		{
+			isConnectionLost=false;
+		}
+		else
+		{
+			
+		 OffLine();
+		 isConnectError=true;
+		 
+		}
+		
+    }, 2000);
+	
+	function OffLine()
+	{
+		if(!isConnectError)
+		{
+		$ionicPopup.alert({
+          title: 'Movie HUB',
+          content: 'Sorry, No Internet Connectivity Now. Please make sure internet connected and Open Movie Hub Again.'
+        })
+        .then(function(result) {
+          if(result) {
+           ionic.Platform.exitApp();
+          }
+        });
+		}
+	}
+	
 });
